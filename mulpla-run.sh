527011915103eca2f399bdac8ae3fef8d4b37ec2
CORDIALCHIP <98459987+CORD1ALCH1P@users.noreply.github.com>
2024-06-03 10:44:34 +0300

    for multi-linux-platform-copability
    CentOS firewall
    Deb firewall
    example.

---
+++ mulpla-run.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install shadowsocks-libev -y
+
+ask_for_input() {
+    local prompt="$1"
+    local default_value="$2"
+    local var
+    while true; do
+        if [ -n "$default_value" ]; then
+            read -rp "$prompt [$default_value]: " var
+            var="${var:-$default_value}"
+        else
+            read -rp "$prompt: " var
+        fi
+
+        if [ -n "$var" ]; then
+            echo "$var"
+            return
+        else
+            echo "invalid input"
+        fi
+    done
+}
+
+server_ip=$(ask_for_input "input Server IP")
+server_port=$(ask_for_input "input Server Port" "8388")
+password=$(ask_for_input "input your Password")
+encryption_method=$(ask_for_input "Encryption Method" "aes-256-cfb")
+firewall_type=$(ask_for_input "wich firewall method" "ufw")
+
+config_json=$(cat <<EOF
+{
+    "server":"$server_ip",
+    "server_port":$server_port,
+    "password":"$password",
+    "timeout":300,
+    "method":"$encryption_method",
+    "fast_open": false
+}
+EOF
+)
+
+config_path="/etc/shadowsocks-libev/config.json"
+echo "$config_json" | sudo tee "$config_path" > /dev/null
+
+sudo systemctl start shadowsocks-libev
+sudo systemctl enable shadowsocks-libev
+
+if ["$fierewall_type" = "ufw" ]; then
+    echo "Настройка ufw"
+    sudo ufw allow $server_port/tcp
+    sudo ufw allow $server_port/udp
+    sudo ufw allow 22/tcp
+    sudo ufw allow 22/udp
+    sudo ufw --force enable
+else
+    sudo firewall-cmd --add-port=8388/tcp --permanent
+    sudo firewall-cmd --add-port=8388/udp --permanent
+    sudo firewall-cmd --add-port=22/tcp --permanent
+    sudo firewall-cmd --add-port=22/udp --permanent
+    sudo firewall-cmd --reload
+
+sudo systemctl restart shadowsocks-libev
+
+echo "complite"
